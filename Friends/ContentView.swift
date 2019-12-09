12188f640f5e79c098b46c93e4909dbfc19c05bd
Mihai Leonte <leontemihai@gmail.com>
2019-12-09 17:49:09 +0200

    Project setup

---
+++ Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Mihai Leonte on 09/12/2019.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
